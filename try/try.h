4be5ac1379501e237af2444b20aad5d94f6e8ec2
arang <2362612521@qq.com>
2016-06-18 23:15:01 +0800

    add

---
+++ try/try.h
@@ -0,0 +1,13 @@
+//
+//  try.h
+//  try
+//
+//  Created by arang on 16/6/18.
+//  Copyright © 2016年 arang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface try : UIView
+
+@end
---
+++ try/try/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  try
+//
+//  Created by arang on 16/6/18.
+//  Copyright © 2016年 arang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
