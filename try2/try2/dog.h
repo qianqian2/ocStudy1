cb83692accb87fcf80af84b3b65becd2035eee2d
arang <2362612521@qq.com>
2016-06-19 08:19:12 +0800

    try

---
+++ try2/try2/dog.h
@@ -0,0 +1,13 @@
+//
+//  dog.h
+//  try2
+//
+//  Created by arang on 16/6/19.
+//  Copyright © 2016年 arang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dog : NSObject
+
+@end
---
+++ try2/try2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  try2
+//
+//  Created by arang on 16/6/19.
+//  Copyright © 2016年 arang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
